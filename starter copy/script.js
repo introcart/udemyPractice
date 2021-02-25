41c1bb470d78cf9c37e244c8c4afb840855e17d2
Steven Carton <spatc@protonmail.com>
2021-02-25 17:05:07 -0500

    finished

--- starter copy/script.js
+++ starter copy/script.js
@@ -22,3 +22,23 @@ const calcTempAmplitude = function (temps) {
 };
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (temps[i] > max) max = curTemp;
+    if (temps[i] < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 7], [9, 4, 5]);
+
+console.log(amplitudeNew);
